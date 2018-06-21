00b77b2ac326e2b136b6408eb8761381db967638
Michael JK <michjk@users.noreply.github.com>
2018-06-21 14:02:46 +0800

    add REST_FRAMEWORK in seeting.py (#9)
    * add REST_FRAMEWORK in seeting.py
    
    * modify REST_FRAMEWORK authentication and permission

--- fundraisee/settings.py
+++ fundraisee/settings.py
@@ -47,6 +47,17 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+   ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
