3e5a52f9b58a7121139a8e25f26dabfdfa6ea7cd
Endilie Yacop Sucipto <endiliey@gmail.com>
2018-07-13 20:34:49 +0800

    add default pagination (#14)

--- fundraisee/settings.py
+++ fundraisee/settings.py
@@ -57,6 +57,8 @@ REST_FRAMEWORK = {
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
    ),
+   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+   'PAGE_SIZE': 50
 }
 
 SWAGGER_SETTINGS = {
